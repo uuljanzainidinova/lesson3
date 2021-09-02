0e3dcb542959d75944aa851d468024f04dbcf3bc
210206uuljanoo <zainidinovauuljan@gmail.com>
2021-09-02 20:51:51 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    
+
+    public static void main(String[] args) {
+        double[] numbers={1.1, 2.2, -3.3, -4.4, 5.5, 6.6, -7.7, 8.8, 9.9, -10.1, 11.2, 12.3, -16.98, 14.1, 15.456
+        };
+        for (double myNumbers:numbers) {
+
+
+            System.out.println((numbers[4]+numbers[5]+numbers[7]+numbers[8]+numbers[10]+numbers[11]+numbers[13]+numbers[14])/8);
+           break;
+
+
+
+        }
+    }
+}
